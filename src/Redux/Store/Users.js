c273674bf6259a0007b3d28d413a3a732fcce04c
amirejokar <amirjowkar2@gmail.com>
2024-05-17 17:43:04 +0330

    folder structuring of stores and redux toolkit

---
+++ src/Redux/Store/Users.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice  = createSlice({
+    name: "users",
+    initialState : [],
+    reducers : {},
+    extraReducers: (builder) => {
+
+    }
+})
+
+export default slice.reducer
---
+++ src/Redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore(
+    {
+        reducer: {
+            users : userReducer,
+        }
+    }
+)
+
+export default store
