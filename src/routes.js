ae8f387cc10801ffdec27332aec7d2552a4b925f
amirejokar <amirjowkar2@gmail.com>
2024-05-22 17:28:37 +0330

    fix home page style with mui

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
-import Home from "./component/Home";
+// import Home from "./component/Home";
 import Users from "./component/Users";
 import Courses from "./component/Courses";
+// import Home from "./component/Home/Home";
+import ButtonBaseDemo from "./component/Home/Home";
 export default [
-    { path: "/", element: <Home /> },
+    { path: "/", element: <ButtonBaseDemo /> },
     { path: "/users", element: <Users /> },
     { path: "/courses", element: <Courses /> },
 ];
