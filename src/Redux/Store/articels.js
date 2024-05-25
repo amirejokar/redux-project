52a4dafbf8068f4972649e5742c0ef897fdad24a
amirejokar <amirjowkar2@gmail.com>
2024-05-25 12:05:58 +0330

    create articels slice and fix it in store

---
+++ src/Redux/Store/articels.js
@@ -0,0 +1,12 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+const slice = createSlice({
+    name : "articls", 
+    initialState : [],
+    reducers : {},
+    extraReducers : (builder) => {
+    }
+})
+
+export default slice.reducer
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from './store/Users'
 import coursesReducer from './store/courses'
+import articlsReducer from './store/articels'
 
 export default configureStore(
     {
         reducer : {
             users : usersReducer,
             courses : coursesReducer,
+            articls : articlsReducer
         }
     }
 )
