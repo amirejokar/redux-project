c6cc6454ab41e412d4f252bf4862764c9f19fd32
amirejokar <amirjowkar2@gmail.com>
2024-05-25 12:12:03 +0330

    fix fetch articles as extra reducer

--- src/Redux/Store/articels.js
+++ src/Redux/Store/articels.js
@@ -1,12 +1,21 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+const fetchArtticles = createAsyncThunk("articles/fetchArtticles", async () => {
+  return fetch("https://redux-cms.iran.liara.run/api/articles")
+    .then((res) => res.json())
+    .then((data) => data);
+});
 
 const slice = createSlice({
-    name : "articls", 
-    initialState : [],
-    reducers : {},
-    extraReducers : (builder) => {
-    }
-})
+  name: "articls",
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(
+      fetchArtticles.fulfilled,
+      (state, action) => action.payload
+    );
+  },
+});
 
-export default slice.reducer
+export default slice.reducer;
