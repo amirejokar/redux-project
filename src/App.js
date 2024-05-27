3039c0aaf90462b81ed2cf33ffee745d24910514
amirejokar <amirjowkar2@gmail.com>
2024-05-27 10:08:53 +0330

    fix header in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import routes from "./routes"
 import { useRoutes } from 'react-router-dom';
-
+import Header from './component/Hearder';
 function App() {
   const router = useRoutes(routes)
   return (
     <div className="App">
+      <Header />
       {router}
     </div>
   );
--- src/component/Home/index.jsx
+++ src/component/Home/index.jsx
@@ -2,12 +2,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { Link } from "react-router-dom";
-import Header from "../Hearder";
 
 export default function Home() {
   return (
     <>
-    <Header/>
     <div className="d-flex justify-content-around mt-4">
       <Link className="btn btn-primary fs-1" to="/users"> users</Link>
       <Link className="btn btn-primary fs-1" to="/courses"> courses</Link>
