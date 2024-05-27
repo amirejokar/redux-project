382c166aaed717379b9940c5df066432bab0df00
amirejokar <amirjowkar2@gmail.com>
2024-05-27 10:39:14 +0330

    fix route of home and header style

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,7 @@
 import React from "react";
 import Users from "./component/Users";
 import Courses from "./component/Courses";
-import Home from "./component/Home"
 export default [
-    { path: "/", element: <Home /> },
     { path: "/users", element: <Users /> },
     { path: "/courses", element: <Courses /> },
 ];
