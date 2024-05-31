81a64848250d726d1bd0c2c743e2a8c5cfc909cc
amirejokar <amirjowkar2@gmail.com>
2024-05-31 15:38:50 +0330

    fix users component

--- src/component/Users/index.jsx
+++ src/component/Users/index.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers , removeUsers } from "../../Redux/store/Users";
-import { addUser } from "../../Redux/store/Users";
+import { fetchUsers, removeUsers } from "../../Redux/store/Users";
+// import { addUser } from "../../Redux/store/Users";
+import {Link} from "react-router-dom"
 import swal from "sweetalert";
+import "./index.css"
 export default function Users() {
   const users = useSelector((state) => state.users);
   const dispath = useDispatch();
 
-  //add new user test 
+  //add new user test
   // const newUser = {
   //   firstname : "amir",
   //   lastname : "jokar",
@@ -19,7 +21,7 @@ export default function Users() {
   // }
 
   // dispath(addUser(newUser))
-  //finish add new user 
+  //finish add new user
 
   useEffect(() => {
     dispath(fetchUsers());
@@ -41,19 +43,70 @@ export default function Users() {
   };
 
   return (
-    <div>
-      <h1>show data of all users</h1>
-      {users.map((user) => {
-        return (
-          <div key={user._id}>
-            <h5 >
-              {user.firstname}"
-              {user.lastname}
-            </h5>
-          <button onClick={() => removeHandler(user._id)}>delete</button>
+    <div className="col-8 content px-0">
+      <div className="content__wrapper">
+        <ul className="content__tabs">
+          <li className="content__tab">
+            <Link to="/users" className="content__tab-link">
+              <span className="fa fa-user"></span>
+              کاربران
+            </Link>
+          </li>
+          <li className="content__tab">
+            <Link to="/infos" className="content__tab-link">
+              <span className="fa fa-book"></span>
+              اطلاعات
+            </Link>
+          </li>
+          <li className="content__tab">
+            <Link to="/courses" className="content__tab-link">
+              <span className="fa fa-store"></span>
+              دوره‌ها
+            </Link>
+          </li>
+
+          <li className="content__tab">
+            <Link to="/articles" className="content__tab-link">
+              <span className="fa fa-newspaper"></span>
+              وبلاگ
+            </Link>
+          </li>
+        </ul>
+
+        <div className="users">
+          <form
+            action="#"
+            className="form row justify-content-between gap-3 mx-0"
+          >
+            <div className="form__box-input col-8 px-0">
+              <span className="fa fa-search form__icon form__icon-search"></span>
+
+              <input
+                type="search"
+                name=""
+                id="search"
+                placeholder="نام یا ایمیل کاربر را وارد کنید "
+                className="form-control form__input"
+                required
+              />
+            </div>
+            <button
+              type="reset"
+              className="btn-custome btn-custome--gray col-3"
+            >
+              حذف کاربر
+            </button>
+          </form>
+
+          <div className="users__list-container">
+            <div className="users__list users__list-wrapper">
+              {/* {users.map((user) => (
+              <UserItem key={user._id} {...user} />
+            ))} */}
+            </div>
           </div>
-        );
-      })}
+        </div>
+      </div>
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import React from "react";
 import Users from "./component/Users";
 import Courses from "./component/Courses";
 export default [
+    { path: "/", element: <Users /> },
     { path: "/users", element: <Users /> },
     { path: "/courses", element: <Courses /> },
 ];
