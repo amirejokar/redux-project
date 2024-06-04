c5ea7b8df15d3083291f9d4ee710e6d753f9b96b
amirejokar <amirjowkar2@gmail.com>
2024-06-04 17:07:58 +0330

    fix articles component and routes

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import React from "react";
 import Users from "./component/Users";
 import Courses from "./component/Courses";
 import Infos from "./component/Info";
+import Articles from "./component/Articles";
 export default [
     { path: "/", element: <Users /> },
     { path: "/users", element: <Users /> },
     { path: "/infos", element: <Infos /> },
     { path: "/courses", element: <Courses /> },
+    { path: "/articles", element: <Articles /> },
 ];
